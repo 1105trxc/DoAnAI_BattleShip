3d77a33c0e9257e78c5ce39a079c22eee033bfc8
Dinh-Duan <phanduan2021vg@gmail.com>
2025-04-11 09:24:11 +0700

    Tao object Ship, Gun, them path cac image

--- main.py
+++ main.py
@@ -7,6 +7,65 @@ pygame.init()
 
 
 #  Game Assets and Objects
+class Ship:
+    def __init__(self, name, img, pos, size, numGuns=0, gunPath=None, gunsize=None, gunCoordsOffset=None):
+        self.name = name
+        self.pos = pos
+        #  Load the Vertical image
+        self.vImage = loadImage(img, size)
+        self.vImageWidth = self.vImage.get_width()
+        self.vImageHeight = self.vImage.get_height()
+        self.vImageRect = self.vImage.get_rect()
+        self.vImageRect.topleft = pos
+        #  Load the Horizontal image
+        self.hImage = pygame.transform.rotate(self.vImage, -90)
+        self.hImageWidth = self.hImage.get_width()
+        self.hImageHeight = self.hImage.get_height()
+        self.hImageRect = self.hImage.get_rect()
+        self.hImageRect.topleft = pos
+        #  Image and Rectangle
+        self.image = self.vImage
+        self.rect = self.vImageRect
+        self.rotation = False
+        #  Ship is current selection
+        self.active = False
+        #  Load gun Images
+        self.gunslist = []
+        if numGuns > 0:
+            self.gunCoordsOffset = gunCoordsOffset
+            for num in range(numGuns):
+                self.gunslist.append(
+                    Guns(gunPath,
+                         self.rect.center,
+                         (size[0] * gunsize[0],
+                          size[1] * gunsize[1]),
+                         self.gunCoordsOffset[num])
+                )
+
+
+    def draw(self, window):
+        """Draw the ship to the screen"""
+        window.blit(self.image, self.rect)
+        for guns in self.gunslist:
+            guns.draw(window, self)
+
+
+class Guns:
+    def __init__(self, imgPath, pos, size, offset):
+        self.orig_image = loadImage(imgPath, size, True)
+        self.image = self.orig_image
+        self.offset = offset
+        self.rect = self.image.get_rect(center=pos)
+
+
+    def update(self, ship):
+        """Updating the Guns Positions on the ship"""
+        self.rect.center = (ship.rect.centerx, ship.rect.centery + (ship.image.get_height()//2 * self.offset))
+
+
+    def draw(self, window, ship):
+        self.update(ship)
+        window.blit(self.image, self.rect)
 
 
 #  Game Utility Functions
@@ -56,6 +115,30 @@ def printGameLogic():
         print(_)
 
 
+def loadImage(path, size, rotate=False):
+    """A function to import the images into memory"""
+    img = pygame.image.load(path).convert_alpha()
+    img = pygame.transform.scale(img, size)
+    if rotate == True:
+        img = pygame.transform.rotate(img, -90)
+    return img
+
+
+def createFleet():
+    """Creates the fleet of ships"""
+    fleet = []
+    for name in FLEET.keys():
+        fleet.append(
+            Ship(name,
+                 FLEET[name][1],
+                 FLEET[name][2],
+                 FLEET[name][3],
+                 FLEET[name][4],
+                 FLEET[name][5],
+                 FLEET[name][6],
+                 FLEET[name][7])
+        )
+    return fleet
 
 
 def updateGameScreen(window):
@@ -63,6 +146,10 @@ def updateGameScreen(window):
 
     #  Draws the player and computer grids to the screen
     showGridOnScreen(window, CELLSIZE, pGameGrid, cGameGrid)
+
+    #  Draw ships to screen
+    for ship in pFleet:
+        ship.draw(window)
 
     pygame.display.update()
 
@@ -84,17 +171,30 @@ pygame.display.set_caption('Battle Ship')
 
 
 #  Game Lists/DIctionaries
-
+FLEET = {
+    'carrier': ['carrier', 'assets/images/ships/carrier/carrier.png', (50, 600), (45, 245),
+                0, '', None, None],
+    'battleship': ['battleship', 'assets/images/ships/battleship/battleship.png', (125, 600), (40, 195),
+                   4, 'assets/images/ships/battleship/battleshipgun.png', (0.4, 0.125), [-0.525, -0.34, 0.67, 0.49]],
+    'destroyer': ['destroyer', 'assets/images/ships/destroyer/destroyer.png', (200, 600), (30, 145),
+                  2, 'assets/images/ships/destroyer/destroyergun.png', (0.5, 0.15), [-0.52, 0.71]],
+    'submarine': ['submarine', 'assets/images/ships/submarine/submarine.png', (275, 600), (30, 145),
+                  1, 'assets/images/ships/submarine/submarinegun.png', (0.25, 0.125), [-0.45]],
+    'patrol boat': ['patrol boat', 'assets/images/ships/patrol boat/patrol boat.png', (350, 600), (20, 95),
+                    0, '', None, None]
+}
 
 #  Loading Game Variables
 pGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (50, 50))
 pGameLogic = createGameLogic(ROWS, COLS)
+pFleet = createFleet()
 
 cGameGrid = createGameGrid(ROWS, COLS, CELLSIZE, (SCREENWIDTH - (ROWS * CELLSIZE), 50))
 cGameLogic = createGameLogic(ROWS, COLS)
+cFleet = None
 
 printGameLogic()
-#  Loading Game Sounds
+#  Loading Game Sounds and Images
 
 
 #  Initialize Players
